3b9201b9c9a226648ebd51a4b44d1683a427ba6b
Jonathan F Visler <levendeslot@gmail.com>
2017-12-14 15:11:47 +0100

    Fixed rotationgrab Gave Big Ball a texture

---
+++ Assets/Scripts/RotationGrab.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.GrabAttachMechanics;
+
+public class RotationGrab : VRTK_TrackObjectGrabAttach {
+    public override void ProcessUpdate() {
+        var rotateForce = trackPoint.position - initialAttachPoint.position;
+        grabbedObject.transform.Rotate(Quaternion.FromToRotation(initialAttachPoint.position - grabbedObject.transform.position, trackPoint.position - grabbedObject.transform.position).eulerAngles, Space.World);
+        //grabbedObjectRigidBody.AddForceAtPosition(rotateForce, initialAttachPoint.position, ForceMode.VelocityChange);
+    }
+
+    protected override void SetTrackPointOrientation(ref Transform trackPoint, Transform currentGrabbedObject, Transform controllerPoint) {
+        trackPoint.position = controllerPoint.position;
+        trackPoint.rotation = controllerPoint.rotation;
+    }
+}
